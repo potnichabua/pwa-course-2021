7e092e87bb8ec5b68315f8c844d9bc8be6f86940
Prikhom <potnicha.bua@gmail.com>
2021-01-30 16:52:45 +0700

    Change text color and background color

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
+        Onion
       </div>
       <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          Line1
+        <div className="App-chatroom-text chat-right">
+          Hello
         </div>
-        <div className="App-chatroom-text">
+        <div className="App-chatroom-text chat-right">
+          I'm Prikhom
+        </div>
+        <div className="App-chatroom-text chat-left">
           Line2
         </div>
       </div>
